d81467add72e0762d771ddc6d987fd4b1e2bc1ce
spartaneman <spartaneman@gmail.com>
2023-07-13 20:59:29 -0400

    finished translation module

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,1 +1,17 @@
+"""Translation methods"""
+
 from deep_translator import MyMemoryTranslator
+
+#Method take in english text and outputs french translated text
+def englishToFrench(englishText):
+    
+    """Use MyMemoryTranslator API to convert english text to french"""
+    frenchText = MyMemoryTranslator(source='auto', target='french').translate(englishText)
+    
+    return frenchText
+
+#Method takes in french text and outputs english translated text
+def frenchToEnglish(frenchText):
+
+    """Use MyMemoryTranslator API to convert french text to english"""
+    englishText = MyMemoryTranslator(source='auto', target='english').translate(frenchText)

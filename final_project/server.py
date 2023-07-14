d8c369d44359e2517763d0106b7a8f93d5bc671d
spartaneman <spartaneman@gmail.com>
2023-07-13 22:08:43 -0400

    Test

--- final_project/server.py
+++ final_project/server.py
@@ -7,8 +7,8 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    french_text = translator.english_to_french(textToTranslate)
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
